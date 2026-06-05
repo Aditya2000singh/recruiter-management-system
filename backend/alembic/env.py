6304fde844240954ce25f6e7aeb1102cf075c4ed
Aditya Kumar Singh <113765987+Aditya2000singh@users.noreply.github.com>
2026-06-06 00:32:09 +0530

    Set SQLAlchemy URL from app configuration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -62,11 +62,20 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+
+    from app.core.config import settings
+
+    config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
     )
+
+    connectable = engine_from_config(
+    config.get_section(config.config_ini_section),
+    prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
+    )
+
 
     with connectable.connect() as connection:
         context.configure(
